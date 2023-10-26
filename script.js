887259f55441227282d301cb7aca25519a191707
maxsegeda <132485806+maxsegeda@users.noreply.github.com>
2023-10-27 00:19:51 +0200

    changes

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ function typeTitle() {
   if (i < title.length) {
     typedTitle.textContent += title.charAt(i);
     i++;
-    setTimeout(typeTitle, 100);
+  } else {
+    // Если достигнут конец строки, сбрасываем счетчик и очищаем текстовое поле
+    i = 0;
+    typedTitle.textContent = "";
   }
 }
 
-typeTitle();
+// Вызываем функцию каждые 100 миллисекунд (или 1 секунда, в зависимости от ваших требований)
+setInterval(typeTitle, 150);
