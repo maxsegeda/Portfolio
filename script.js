4d6aa4f104028d9821b937ad68bbfbed21e6b0fd
maxsegeda <132485806+maxsegeda@users.noreply.github.com>
2024-01-08 21:10:31 +0100

    calculator

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-const title = "Full-stack Developer";
+const title = "JavaScript Developer";
 const typedTitle = document.getElementById("typed-title").querySelector("h2");
-
 let i = 0;
+let isDeleting = false;
 
 function typeTitle() {
-  if (i < title.length) {
-    typedTitle.textContent += title.charAt(i);
+  const currentText = typedTitle.textContent;
+
+  if (!isDeleting && i <= title.length) {
+    typedTitle.textContent = title.substring(0, i);
     i++;
-  } else {
-    i = 0;
-    typedTitle.textContent = "";
+
+    if (i === title.length + 1) {
+      isDeleting = true;
+      setTimeout(() => {
+        isDeleting = true;
+        typeTitle();
+      }, 1000);
+    } else {
+      setTimeout(typeTitle, 180);
+    }
+  } else if (isDeleting && i >= 0) {
+    typedTitle.textContent = title.substring(0, i);
+    i--;
+
+    if (i === 0) {
+      isDeleting = false;
+      typeTitle();
+    } else {
+      setTimeout(typeTitle, 60);
+    }
   }
 }
 
-setInterval(typeTitle, 150);
+typeTitle();
